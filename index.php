ea8f0b99d988a52e105448001ed0b2d1898f7e2a
Mubassir Hayat <mubassirhayat@gmail.com>
2014-05-11 05:20:17 -0700

    Twitter Feed Added

--- index.php
+++ index.php
@@ -135,6 +135,28 @@
                             </p>
 						</blockquote>
 					</div>
+                    <hr>
+					<!-- End Blockquote -->
+                    
+                    <!-- Start Blockquote -->
+					<div class="span6" align="center">
+						<blockquote>
+							<p>
+                            	<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/MessiahApp"  data-widget-id="465463936993615873">Tweets by @MessiahApp</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                            </p>
+						</blockquote>
+					</div>
+					<!-- End Blockquote -->
+                    <!-- Start Blockquote -->
+					<div class="span6" align="center">
+						<blockquote>
+							<p>
+                            	<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=%23MessiahApp"  data-widget-id="465465889471475712">Tweets about "#MessiahApp"</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                            </p>
+						</blockquote>
+					</div>
 					<!-- End Blockquote -->
 				</div>
 				<!-- End BlockQuote/Tooltip Section -->
