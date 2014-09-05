c0e2747201d2bcb6af26a0bbd2a9616898c7c79b
Mubassir Hayat <mubassirhayat@gmail.com>
2014-09-05 15:11:29 +0500

    Server credentials changed

--- inc/ClassLibrary/Config.php
+++ inc/ClassLibrary/Config.php
@@ -3,10 +3,15 @@
   namespace ClassLibrary;
 
   class Config {
-      public static $DB_SERVER = "localhost";
-      public static $DB_USER = "root";
-      public static $DB_PASS = "";
-      public static $MESSIAH_DB = "messiah";
+      // public static $DB_SERVER = "localhost";
+      // public static $DB_USER = "root";
+      // public static $DB_PASS = "";
+      // public static $MESSIAH_DB = "messiah";
+
+      public static $DB_SERVER = "mysql8.000webhost.com";
+      public static $DB_USER = "a2186923_root";
+      public static $DB_PASS = "911EbtiZaidi";
+      public static $MESSIAH_DB = "a2186923_messiah";
 
       public static $GOOGLE_API_KEY = "AIzaSyDE_ntZqfy1E2HRZ6jOfZZKYe7M7WJyT7Q"; // Place your Google API Key
   }
