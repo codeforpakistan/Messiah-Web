029dc48ef130e8394c29af4fdba928ee666aecd9
Mubassir Hayat <mubassirhayat@gmail.com>
2014-09-08 13:47:00 +0500

    Remote server settings

--- inc/ClassLibrary/Config.php
+++ inc/ClassLibrary/Config.php
@@ -8,10 +8,10 @@
       // public static $DB_PASS = "";
       // public static $MESSIAH_DB = "messiah";
 
-      public static $DB_SERVER = "mysql8.000webhost.com";
-      public static $DB_USER = "a2186923_root";
-      public static $DB_PASS = "911EbtiZaidi";
-      public static $MESSIAH_DB = "a2186923_messiah";
+      public static $DB_SERVER = "107.170.26.223";
+      public static $DB_USER = "root";
+      public static $DB_PASS = "password";
+      public static $MESSIAH_DB = "messiah";
 
       public static $GOOGLE_API_KEY = "AIzaSyDE_ntZqfy1E2HRZ6jOfZZKYe7M7WJyT7Q"; // Place your Google API Key
   }
