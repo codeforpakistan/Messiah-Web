afac3402d3fa7ac51f7ab40745b2adfcf8107577
Mubassir Hayat <mubassirhayat@gmail.com>
2014-09-09 17:27:58 +0500

    cleardb settings

--- inc/ClassLibrary/Config.php
+++ inc/ClassLibrary/Config.php
@@ -8,10 +8,11 @@
       // public static $DB_PASS = "";
       // public static $MESSIAH_DB = "messiah";
 
-      public static $DB_SERVER = "107.170.26.223";
-      public static $DB_USER = "root";
-      public static $DB_PASS = "password";
-      public static $MESSIAH_DB = "messiah";
+      public static $URL = parse_url(getenv("CLEARDB_DATABASE_URL"));
+      public static $DB_SERVER = $url["host"];
+      public static $DB_USER = $url["user"];
+      public static $DB_PASS = $url["pass"];
+      public static $MESSIAH_DB = substr($url["path"],1);
 
       public static $GOOGLE_API_KEY = "AIzaSyDE_ntZqfy1E2HRZ6jOfZZKYe7M7WJyT7Q"; // Place your Google API Key
   }
